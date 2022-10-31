06a0cbde68e4f1728a4be3934991982f74e82424
Jeevanandam Perumal <jeevanandam.sf3935@gmail.com>
2022-10-31 18:43:38 +0530

    Added simple code to the .net core mvc

---
+++ Assignment/Employee/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment.Controllers
+{
+    public class HomeController
+    {
+        public string Index()
+        {
+            return "Hello from mvc";
+        }
+    }
+}
